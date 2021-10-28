71b84b5cf1bb7a7981b5a8ae7bfb7763cd815a0c
myzozoz <markusp_holopainen@yahoo.com>
2021-10-28 19:55:05 +0300

    Added WordCloud implementation

--- pages/results.tsx
+++ pages/results.tsx
@@ -1,5 +1,8 @@
 import type { NextPage } from 'next'
 import { GetServerSideProps } from 'next'
+import Link from 'next/link'
+
+import { TagCloud } from 'react-tagcloud'
 
 type Props = {
   results: any,
@@ -7,13 +10,25 @@ type Props = {
 
 const ResultsPage: NextPage<Props> = ({results}) => {
   console.log(results)
+  const data = results.top_keywords || []
+
   return (
-    <div>results: </div>
+    <div>
+      <TagCloud
+        minSize={12}
+        maxSize={100}
+        tags={data}
+        onClick={tag => console.log(`'${tag.value}' was clicked!`)}/>
+      <Link href='/'>
+        <button>Go Back</button>
+      </Link>
+    </div>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async (context)  => {
-  const res = await fetch('https://tagspy-service.herokuapp.com/api/games?tags='+context.query.tags)
+  const url_base = process.env.NODE_ENV === 'development' ? "http://localhost:5000/api/games?tags=" : "https://tagspy-service.herokuapp.com/api/games?tags="
+  const res = await fetch(url_base + context.query.tags)
   const data = await res.text()
 
   return {
